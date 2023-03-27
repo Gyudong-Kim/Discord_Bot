e0728ae535241c168180979c94a2746d7ea2fae5
Gyudong-Kim <dong3955@gmail.com>
2023-03-27 15:50:32 +0900

    [STUDY] 디스코드 봇에 명령어 추가

---
+++ Study/Chapter02/main.py
@@ -0,0 +1,24 @@
+# 2.2. 봇에 명령어 추가
+
+import discord
+from discord.ext import commands
+
+
+def main():
+    prefix = "!"  # 접두사 설정
+    intents = discord.Intents.all()  # 서버 멤버의 정보나, 멤버 리스트를 불러올 수 있도록 허용
+
+    client = commands.Bot(command_prefix=prefix, intents=intents)  # 봇 객체 생성
+
+    @client.command(name="ping")
+    async def _ping(ctx):
+        await ctx.send("pong!")
+
+    with open("token.txt", encoding="utf-8") as file:
+        token = file.read()
+
+    client.run(token)
+
+
+if __name__ == "__main__":
+    main()
